412086545d4b7e8f4439746e69e13714d86ea1a1
Thomas <thomtrepanier@gmail.com>
2020-09-14 13:15:35 -0400

    Exos done and passed

--- exercice.py
+++ exercice.py
@@ -3,32 +3,46 @@
 
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	return voltage**2/resistance
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
 	v1[0] # Pour accéder au X
 	v1[1] # Pour accéder au Y
-	pass
+	return v1[0]*v2[0] + v1[1]*v2[1] == 0
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	sum = 0
+	neg = 0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if(v < 0):
+			neg += 1
+			continue
+		sum += v
+	return sum / (len(values) - neg)
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
+	twenties = 0
+	tens = 0
+	fives = 0
+	ones = 0
 	while value != 0:
 		if value >= 20:
-			pass
+			twenties += 1
+			value -= 20
 		elif value >= 10:
-			pass
+			tens += 1
+			value -= 10
 		elif value >= 5:
-			pass
+			fives += 1
+			value -= 5
 		elif value >= 1:
-			pass
+			ones += 1
+			value -= 1
 
-	return (twenties, tens, fives, twos, ones);
+	return (twenties, tens, fives, ones);
 
 if __name__ == "__main__":
 	print(dissipated_power(69, 420))
